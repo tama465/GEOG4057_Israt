17d0de526b760dfb75f982b71737fee3e8a108a5
tama465 <itama1@lsu.edu>
2024-09-18 09:41:10 -0500

    Update

--- Assignment_02/buffer.py
+++ Assignment_02/buffer.py
@@ -1,7 +1,8 @@
+
 import arcpy
 arcpy.analysis.Buffer(
     in_features="no_retail.shp",
-    out_feature_class="./retail_buffer.shp",
+    out_feature_class="./retail_buffer3.shp",
     buffer_distance_or_field="500 Meters",
     line_side="FULL",
     line_end_type="ROUND",
